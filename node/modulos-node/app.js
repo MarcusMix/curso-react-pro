d19c0d72a09f2610d48978c35cbc87102dc58e2f
Marcus Sandi <tradedomarcus@gmail.com>
2022-09-16 15:47:05 -0300

    install & import chalk

--- node/modulos-node/app.js
+++ node/modulos-node/app.js
@@ -1,4 +1,5 @@
-const validator = require('validator');
+import validator from 'validator';
+import chalk from 'chalk';
 
 const email = 'johnosso@gol.com.br'
 
@@ -6,3 +7,12 @@ const isEmailValid = validator.isEmail(email);
 
 console.log(isEmailValid)
 
+
+const log = console.log;
+
+// const letraColorida = 'OI, sou um texto de cor verde!'
+
+// log(chalk.blue('OI, sou um texto de cor verde!'))
+
+console.log(chalk.bold.green("Hello World!"))
+const oi = log(chalk.bold.bgWhite('valeu meu chapa'))

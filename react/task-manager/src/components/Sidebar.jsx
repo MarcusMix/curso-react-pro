843a1e76ad781d9d6191ab44a0ab1ff1a6f01c19
Marcus Sandi <tradedomarcus@gmail.com>
2022-09-23 14:54:24 -0300

    feat: add sidebar component

---
+++ react/task-manager/src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import CustomButton from './CustomButton'
+
+import logo from '../assets/images/logo.png'
+import './Sidebar.scss'
+
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar-container">
+           <div className="logo">
+                <img src={logo} alt="logo" />
+           </div>
+
+           <div className="sign-out">
+                <CustomButton> 
+                    Sair
+                </CustomButton>
+           </div>
+        </div>
+    )
+}
+
+export default Sidebar

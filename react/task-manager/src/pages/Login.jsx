3268d74ba0503ca87857e8e0899cbb3a7122ce44
Marcus Sandi <tradedomarcus@gmail.com>
2022-09-26 13:39:39 -0300

    feat: add navigation to home page

--- react/task-manager/src/pages/Login.jsx
+++ react/task-manager/src/pages/Login.jsx
@@ -1,14 +1,24 @@
 import logo from '../assets/images/logo-2.png'
+
 import CustomButton from '../components/CustomButton'
+
 import './Login.scss'
 
+import {useNavigate} from 'react-router-dom'
+
 const Login = () => {
+    const navigate = useNavigate()
+
+    const handleSignInClick = () => {
+        navigate('/')
+    }
+
     return (
         <div className="login-container">
             <img src={logo} alt="logo" />
 
             <div className="button-container">
-                <CustomButton>
+                <CustomButton onClick={handleSignInClick}>
                     Entrar
                 </CustomButton>
             </div>

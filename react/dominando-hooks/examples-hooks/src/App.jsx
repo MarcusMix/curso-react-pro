bd49a0412596be2e9c072ee09e4b1a5c3839f024
Marcus Sandi <tradedomarcus@gmail.com>
2022-09-29 17:03:12 -0300

    feat: using useEffect in practice

--- react/dominando-hooks/examples-hooks/src/App.jsx
+++ react/dominando-hooks/examples-hooks/src/App.jsx
@@ -1,21 +1,29 @@
 import './App.scss';
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [state, setState] = useState(0);
+  const [resourseType, setResourseType] = useState('posts');
 
-  const incrementCount = () => {
-    setState(prevState => prevState + 1)
+  const changeResourceType = (resourseType) => {
+    setResourseType(resourseType)
   }
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourseType}`)
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+
+  }, [resourseType])
 
   return (
     <>
       <div>
-        <p>Se voce clicar no botão {state}, o numero irá mudar</p>
-        <div className='btn'>
-          <button onClick={incrementCount} >
-            clica aqui bro
-          </button>
+        <h1>{resourseType}</h1>
+        <div>
+          <button onClick={() => changeResourceType('posts')}>Posts</button>
+          <button onClick={() => changeResourceType('comments')}>Comments</button>
+          <button onClick={() => changeResourceType('todos')}>ToDos</button>
         </div>
       </div>
     </>

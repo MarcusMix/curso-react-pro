8e273a21d8ef83114f93fa72c89eadd8f82f2edb
Marcus Sandi <tradedomarcus@gmail.com>
2022-10-03 15:01:35 -0300

    types and interfaces in typescript converted in javascript

---
+++ fsc-typescript/dist/types-and-interfaces.js
@@ -0,0 +1,63 @@
+"use strict";
+const user = {
+    id: 1,
+    name: 'Marcus',
+    email: 'email@email.com',
+    age: 21 // opcional
+};
+const user2 = {
+    id: 2,
+    name: 'Milena',
+    email: 'email@email.com'
+};
+const users = [
+    {
+        id: 2,
+        name: 'Milena',
+        email: 'email@email.com'
+    },
+    {
+        id: 1,
+        name: 'Marcus',
+        email: 'email@email.com',
+        age: 21
+    }
+];
+const author2 = {
+    name: 'Jesse Pinkman',
+    id: 69,
+    email: 'jessehomemrosa@gmail.com',
+    books: ['Fuma é baum', 'Como fazer metanfetamina com agua e sal']
+};
+const user3 = {
+    id: 1,
+    name: 'Marcus',
+    email: 'email@email.com'
+};
+const add = (x, y) => {
+    return x + y;
+};
+const sub = (x, y) => {
+    return x - y;
+};
+const user10 = {
+    id: 1,
+    name: 'Marcus',
+    register() {
+        return 'Registrado com sucesso!';
+    }
+};
+const author = {
+    name: 'Walter White',
+    id: 2,
+    email: 'walterbranco@gmail.com',
+    books: ['Better Call Saul', 'Breaking Bad']
+};
+const author3 = {
+    books: ['Los pollos hermanos e a droga', 'Melhor ligar pro Sal']
+};
+const author4 = {
+    name: 'Mike',
+    id: 7,
+    email: 'mikeehrmantraut@gmail.com'
+};

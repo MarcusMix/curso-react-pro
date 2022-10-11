d6a79d82baa241c9fe58d5d9ab2f66ab5dee938a
Marcus Sandi <tradedomarcus@gmail.com>
2022-10-11 16:16:22 -0300

    feat: update HomePage Component

--- ecommerce/src/pages/home/home.page.tsx
+++ ecommerce/src/pages/home/home.page.tsx
@@ -1,11 +1,15 @@
 
 //Components
+import Categories from "../../components/categories/categories-component"
 import Header from "../../components/header/header.component"
 
 const HomePage = () => {
     return (
         <div>
-            <Header/>
+            <Header />
+
+            <Categories />
+
         </div>
     )
 }

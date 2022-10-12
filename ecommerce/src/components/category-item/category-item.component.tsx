a2500b4d939891673faf152803c3a91dc42f74fc
Marcus Sandi <tradedomarcus@gmail.com>
2022-10-12 20:39:34 -0300

    feat: add Category Item Component

---
+++ ecommerce/src/components/category-item/category-item.component.tsx
@@ -0,0 +1,25 @@
+import { FunctionComponent } from 'react'
+
+//Ultilities
+import Category from "../../types/category.types"
+
+//Styles
+import './category-item.style.css'
+
+interface CategoryItemProps {
+    category: Category
+}
+
+const CategoryItem: FunctionComponent<CategoryItemProps> = ({ category }) => {
+    return (
+        <div className="category-item-container" 
+            style={{ backgroundImage: category.imageURL }}>
+            <div className="category-name">
+                <p>{category.displayName}</p>
+                <p>Explorar</p>
+            </div>
+        </div>
+    )
+}
+
+export default CategoryItem

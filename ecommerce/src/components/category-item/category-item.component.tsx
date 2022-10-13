ea32b5d3d0d2ca816ef43266c1d7abac48afe66a
Marcus Sandi <tradedomarcus@gmail.com>
2022-10-12 21:00:47 -0300

    fix: update Category Item background item

--- ecommerce/src/components/category-item/category-item.component.tsx
+++ ecommerce/src/components/category-item/category-item.component.tsx
@@ -12,8 +12,8 @@ interface CategoryItemProps {
 
 const CategoryItem: FunctionComponent<CategoryItemProps> = ({ category }) => {
     return (
-        <div className="category-item-container" 
-            style={{ backgroundImage: category.imageURL }}>
+        <div className="category-item-container"
+            style={{ backgroundImage: `url('{category.imageURL}')` }}>
             <div className="category-name">
                 <p>{category.displayName}</p>
                 <p>Explorar</p>

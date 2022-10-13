ec9ff153e9b1feba32736b56c9baeb016788f3f7
Marcus Sandi <tradedomarcus@gmail.com>
2022-10-12 21:01:12 -0300

    feat: show CategoryItem for each Category

--- ecommerce/src/components/categories/categories-component.tsx
+++ ecommerce/src/components/categories/categories-component.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
+//Components
+import CategoryItem from '../category-item/category-item.component'
+
 // Ultilities
 import Category from '../../types/category.types'
 import env from '../../config/env.config'
@@ -34,7 +37,11 @@ const Categories = () => {
     return (
         <div className="categories-container">
             <div className="categories-content">
-
+                {categories.map(category => (
+                    <div key={category.id}>
+                        <CategoryItem category={category} />
+                    </div>
+                ))}
             </div>
         </div>
     )

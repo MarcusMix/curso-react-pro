bd900296e9d9c80763b021b49bfc9e61b551fc49
Marcus Sandi <tradedomarcus@gmail.com>
2022-10-17 14:23:00 -0300

    refactor: add Styled Components to Category Item Components

--- ecommerce/src/components/category-item/category-item.component.tsx
+++ ecommerce/src/components/category-item/category-item.component.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent } from 'react'
 
 //Ultilities
 import Category from "../../types/category.types"
+import { CategoryItemContainer, CategoryName } from './category-item'
 
 //Styles
 import './category-item.style.css'
@@ -12,13 +13,12 @@ interface CategoryItemProps {
 
 const CategoryItem: FunctionComponent<CategoryItemProps> = ({ category }) => {
     return (
-        <div className="category-item-container"
-            style={{ backgroundImage: `url('{category.imageURL}')` }}>
-            <div className="category-name">
+        <CategoryItemContainer backgroundImage={category.imageURL}>
+            <CategoryName>
                 <p>{category.displayName}</p>
                 <p>Explorar</p>
-            </div>
-        </div>
+            </CategoryName>
+        </CategoryItemContainer>
     )
 }
 

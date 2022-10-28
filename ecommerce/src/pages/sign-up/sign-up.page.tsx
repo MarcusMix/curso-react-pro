1a6f34ae4e6da568ed9894884bf5bda499981b59
Marcus Sandi <tradedomarcus@gmail.com>
2022-10-28 16:03:39 -0300

    feat: add user account creation

--- ecommerce/src/pages/sign-up/sign-up.page.tsx
+++ ecommerce/src/pages/sign-up/sign-up.page.tsx
@@ -1,6 +1,8 @@
 import { FiLogIn } from 'react-icons/fi'
 import { useForm } from 'react-hook-form'
 import validator from 'validator'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { addDoc, collection } from 'firebase/firestore'
 
 //Components
 import CustomButton from "../../components/custom-button/custom-button.component"
@@ -11,8 +13,11 @@ import InputErrorMessage from '../../components/input-error-message/input-error-
 //Styles
 import { SignUpContainer, SignUpContent, SignUpHeadline, SignUpInputContainer } from "./sign-up.styles"
 
+//Ultilities
+import { auth, db } from '../../config/firebase.config'
+
 interface SignUpForm {
-    name: string;
+    firstName: string;
     lastName: string;
     email: string;
     password: string;
@@ -30,8 +35,20 @@ const SignUpPage = () => {
 
     const watchPassword = watch('password')
 
-    const handleSubmitPress = (data: SignUpForm) => {
-        console.log({ data })
+    const handleSubmitPress = async (data: SignUpForm) => {
+        try {
+            const userCrendentials = await createUserWithEmailAndPassword(auth, data.email, data.password)
+
+
+            await addDoc(collection(db, 'users'), {
+                id: userCrendentials.user.uid,
+                email: userCrendentials.user.email,
+                firstName: data.firstName,
+                lastName: data.lastName
+            })
+        } catch (error) {
+            console.log(error)
+        }
     }
 
 
@@ -45,12 +62,12 @@ const SignUpPage = () => {
                     <SignUpInputContainer>
                         <p>Nome</p>
                         <CustomInput
-                            hasError={!!errors?.name}
+                            hasError={!!errors?.firstName}
                             placeholder="Digite seu nome"
-                            {...register('name', { required: true })}
+                            {...register('firstName', { required: true })}
                         />
 
-                        {errors?.name?.type === 'required' && (
+                        {errors?.firstName?.type === 'required' && (
                             <InputErrorMessage>O nome é obrigatório.</InputErrorMessage>
                         )}
 

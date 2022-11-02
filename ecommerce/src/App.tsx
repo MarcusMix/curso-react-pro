c7092519f1d8f95f3633ba2b80756e4c77aa7ec5
Marcus Sandi <tradedomarcus@gmail.com>
2022-11-02 20:45:08 -0300

    feat: add initializing state to app

--- ecommerce/src/App.tsx
+++ ecommerce/src/App.tsx
@@ -1,6 +1,6 @@
 import { onAuthStateChanged } from 'firebase/auth'
 import { collection, getDocs, query, where } from 'firebase/firestore'
-import { FunctionComponent, useContext } from 'react'
+import { FunctionComponent, useContext, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // Componentes
@@ -14,13 +14,16 @@ import { auth, db } from './config/firebase.config'
 
 const App: FunctionComponent = () => {
 
+  const [isInitializing, setInitializing] = useState(true);
+
   const { isAuthenticated, loginUser, logoutUser } = useContext(UserContext)
 
   onAuthStateChanged(auth, async (user) => {
     const isSigningOut = isAuthenticated && !user
 
     if (isSigningOut) {
-      return logoutUser()
+      logoutUser()
+      return setInitializing(false)
     }
     const isSigningIn = !isAuthenticated && user
 
@@ -32,9 +35,15 @@ const App: FunctionComponent = () => {
       const userFromFirestore = querySnapshot.docs[0]?.data()
 
 
-      return loginUser(userFromFirestore as any)
+      loginUser(userFromFirestore as any)
+
+      return setInitializing(false)
     }
+
+    return setInitializing(false)
   })
+
+  if(isInitializing) return null
 
   return (
     <BrowserRouter>

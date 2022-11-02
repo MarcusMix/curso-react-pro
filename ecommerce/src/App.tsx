082df5c147f576070ab3d9239afb8a3e60669521
Marcus Sandi <tradedomarcus@gmail.com>
2022-11-02 14:13:12 -0300

    fix: remove param of useHref

--- ecommerce/src/App.tsx
+++ ecommerce/src/App.tsx
@@ -1,6 +1,6 @@
 import { onAuthStateChanged } from 'firebase/auth'
 import { FunctionComponent } from 'react'
-import { BrowserRouter, Routes, Route, useHref } from 'react-router-dom'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import { auth } from './config/firebase.config'
 
 // Componentes
@@ -9,7 +9,7 @@ import LoginPage from './pages/login/login.page'
 import SignUpPage from './pages/sign-up/sign-up.page'
 
 const App: FunctionComponent = () => {
-  
+
   onAuthStateChanged(auth, (user => {
     console.log(user)
   }))

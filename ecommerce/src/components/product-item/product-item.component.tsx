212eb171d2348174a0529fa04706ee21e885f65c
Marcus Sandi <tradedomarcus@gmail.com>
2022-11-20 14:04:39 -0300

    feat: add Product Item Component

--- ecommerce/src/components/product-item/product-item.component.tsx
+++ ecommerce/src/components/product-item/product-item.component.tsx
@@ -1,15 +1,25 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useContext } from "react";
 import Product from "../../types/product.types";
+import CustomButton from "../custom-button/custom-button.component";
 import { ProductContainer, ProductImage, ProductInfo } from "./product-item.styles";
+import { BsCartPlus } from 'react-icons/bs'
+import { CartContext } from "../../contexts/cart.context";
 
 interface ProductItemProps {
     product: Product
 }
 
 const ProductItem: FunctionComponent<ProductItemProps> = ({ product }) => {
+    const { addProductsToCart } = useContext(CartContext)
+
+    const handleAddProductsToCartClick = () => {
+        addProductsToCart(product)
+    }
     return (
         <ProductContainer>
-            <ProductImage imageUrl={product.imageUrl} />
+            <ProductImage imageUrl={product.imageUrl}>
+                <CustomButton startIcon={<BsCartPlus />} onClick={handleAddProductsToCartClick}>Adicionar ao carrinho</CustomButton>
+            </ProductImage>
 
 
             <ProductInfo>

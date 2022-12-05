34c8b4c593b88d2d832140a8fcefadb6b05f0526
Marcus Sandi <tradedomarcus@gmail.com>
2022-12-04 21:49:33 -0300

    feat: organize import react router dom

--- ecommerce/src/pages/login/login.page.tsx
+++ ecommerce/src/pages/login/login.page.tsx
@@ -6,13 +6,13 @@ import { AuthError, AuthErrorCodes, signInWithEmailAndPassword, signInWithPopup 
 import { addDoc, collection, getDocs, query, where } from 'firebase/firestore'
 import { useEffect, useContext, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 //Components
 import CustomButton from "../../components/custom-button/custom-button.component"
 import CustomInput from '../../components/custom-input/custom-input.component'
 import Header from "../../components/header/header.component"
 import InputErrorMessage from '../../components/input-error-message/input-error-message.component'
-import { useNavigate } from 'react-router-dom'
 import Loading from '../../components/loading/loading.components'
 
 //Styles

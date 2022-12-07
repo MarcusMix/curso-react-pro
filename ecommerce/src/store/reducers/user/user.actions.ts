2d2a4f22ef3473f15d1851983da1404cb4295141
Marcus Sandi <tradedomarcus@gmail.com>
2022-12-06 22:59:27 -0300

    feat: add interface and alter name logout for logoutUser

--- ecommerce/src/store/reducers/user/user.actions.ts
+++ ecommerce/src/store/reducers/user/user.actions.ts
@@ -1,11 +1,23 @@
 import User from "../../../types/user.types";
 import UserActionTypes from "./user.action-types";
 
-export const loginUser = (payload: User) => ({
+interface LoginUserAction {
+    type: typeof UserActionTypes.LOGIN
+    payload: User
+}
+
+interface LogoutUserAction {
+    type: typeof UserActionTypes.LOGOUT
+}
+
+
+export const loginUser = (payload: User): LoginUserAction => ({
     type: UserActionTypes.LOGIN,
     payload
 })
 
-export const logout = () => ({
+export const logoutUser = (): LogoutUserAction => ({
     type: UserActionTypes.LOGOUT
 })
+
+export type UserActions = LoginUserAction | LogoutUserAction

70d58893dc712492722d5eed309b88b5afa65be1
Marcus Sandi <tradedomarcus@gmail.com>
2022-12-07 16:58:18 -0300

    feat: added filter on fuction addProductToCart

--- ecommerce/src/store/reducers/cart/cart.reducer.ts
+++ ecommerce/src/store/reducers/cart/cart.reducer.ts
@@ -19,9 +19,28 @@ const cartReducer = (state = initialState, action: any) => {
     switch (action.type) {
         case CartActionType.toggleCart:
             return { ...state, isVisible: !state.isVisible }
+        case CartActionType.addProductToCart: {
+            const product = action.payload;
+
+            //se o produto ja está no carrinho
+            const productIsAlreadyInCart = state.products.some((item) => item.id === product.id)
+
+            //se sim, aumentar sua quantidade
+            if (productIsAlreadyInCart) {
+                return {
+                    ...state,
+                    products: state.products.map((item) =>
+                        item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item)
+                }
+            }
+            
+            //se não, adicionar
+            return { ...state, products: [...state.products, { ...product, quantity: 1 }] }
+        }
         default:
             return { ...state }
     }
+    
 }
 
 export default cartReducer

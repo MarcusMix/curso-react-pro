8e24cebe0b497723ec16cd552b6b89dd38042993
Marcus Sandi <tradedomarcus@gmail.com>
2022-12-07 16:57:46 -0300

    feat:  added addProductToCart type

--- ecommerce/src/store/reducers/cart/cart.actions.ts
+++ ecommerce/src/store/reducers/cart/cart.actions.ts
@@ -1,3 +1,12 @@
+
+import Product from "../../../types/product.types"
+import CartActionType from "./cart.action-types"
+
 export const toggleCart = () => ({
-    type: 'cart/toggle'
+    type: CartActionType.toggleCart
 })
+
+export const addProductToCart = (payload: Product) => ({
+    type: CartActionType.addProductToCart,
+    payload
+}) 

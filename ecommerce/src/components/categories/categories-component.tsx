d767f47a53299f0853dd3b138e833cd154524866
Marcus Sandi <tradedomarcus@gmail.com>
2022-12-18 17:14:30 -0300

    feat: remove context api and add redux methods

--- ecommerce/src/components/categories/categories-component.tsx
+++ ecommerce/src/components/categories/categories-component.tsx
@@ -1,32 +1,33 @@
-import { useContext, useEffect } from 'react'
-import { CategoryContext } from '../../contexts/category.context'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
+//Ultilities
+import { fetchCategories } from '../../store/reducers/category/category.actions'
 
 //Components
 import CategoryItem from '../category-item/category-item.component'
+import { useAppSelector } from '../hooks/redux.hooks'
 import Loading from '../loading/loading.components'
-
-
-
 
 //Styles
 import { CategoriesContainer, CategoriesContent } from './categories-styles'
 
 
 const Categories = () => {
-    const { categories, isLoading, fetchCategories } = useContext(CategoryContext)
 
+    const { isLoading, categories } = useAppSelector(state => state.categoryReducer)
 
+    const dispatch = useDispatch()
 
 
     useEffect(() => {
-        fetchCategories()
+        dispatch(fetchCategories() as any)
     }, [])
 
 
     return (
         <CategoriesContainer>
-            {isLoading && <Loading/>}
+            {isLoading && <Loading />}
             <CategoriesContent>
                 {categories.map((category) => (
                     <div key={category.id}>
